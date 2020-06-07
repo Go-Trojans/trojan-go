19a1a38c5ab064853e568c38bc1883d6d743d2ff
CSCI561-Go <rupeshsjce@gmail.com>
2020-06-07 11:33:32 +0530

    Rupesh

--- code/algos/encoders/trojangoPlane.py
+++ code/algos/encoders/trojangoPlane.py
@@ -2,3 +2,10 @@
 Please keep the code here for the input features.
 
 """
+
+import numpy as np
+
+class TrojanGoPlane(Encoder):
+	def __init__(self, board_size, plane_size):
+		self.board_width, self.board_height = board_size
+		self.num_planes = plane_size
