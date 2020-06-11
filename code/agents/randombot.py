26b4ca6b10f20d3cb478dc1d3c3cab049a199c68
CSCI561-Go <rupeshsjce@gmail.com>
2020-06-11 17:54:21 +0530

    Randombot implementation

---
+++ code/agents/randombot.py
@@ -0,0 +1,11 @@
+import random
+from godomain import Move
+from algo.encoders import Point
+
+__all__ = ['RandomBot']
+
+
+class RandomBot(Agent):
+    print("RandomBot selecting move ...")
+    def select_move(self, game_state): 
+        raise NotImplementedError() 
