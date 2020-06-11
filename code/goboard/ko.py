ad8fd2fb29a1ecab1dfcb413aef6bb7b7b9305d6
Raveena Kshatriya <raveenak96@gmail.com>
2020-06-10 21:05:23 -0700

    Update ko.py

--- code/goboard/ko.py
+++ code/goboard/ko.py
@@ -12,7 +12,7 @@ def check_ko(row,col,board,previous_board,piece_type,board_dim) :
     dead_stones = []
     for i in range(board_dim):
         for j in range(board_dim):
-            if previous_board.state[i][j] == piece_type and board[i][j] != piece_type:
+            if previous_board[i][j] == piece_type and board[i][j] != piece_type:
                 dead_stones.append((i, j))
     if dead_stones and compare_board(previous_board, test_board) :
         return False
