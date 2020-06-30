6f47b4955d062dac387a2ce2b109d5ab967a09a8
puranjayr96 <puranjayr.pr@gmail.com>
2020-06-29 21:09:56 -0700

    Add files via upload

--- code/algos/mcts/mcts.py
+++ code/algos/mcts/mcts.py
@@ -82,33 +82,37 @@ class MCTSPlayer :
         self.player = player
 
     def uct_select_move(gameState,simulations,nn,verbose = False):
-		 """ Takes in a GameState object representing the current game state and selects the best move using MCTS.Returns a Move object.Provides optional parameter to save the move to disk ( for use in self-play for training neural network)
+         """ Takes in a GameState object representing the current game state and selects the best move using MCTS.Returns a Move object.Provides optional parameter to save the move to disk ( for use in self-play for training neural network)
         """
          """
         Conduct a tree search for itermax iterations starting from gameState.
         Return the best move from the gameState. Assumes 2 alternating players(player 1 starts), with game results in the range[-1, 1].
         """
         rootnode = MCTSNode(state = gameState)
+        visited = set()
         for i in range(itermax):
             node = rootnode
             state = copy.deepcopy(gameState)
             # Select
-            while not node.untriedMoves and node.childNodes: # node is fully expanded and non-terminal
+            while node.state in visited: # node is fully expanded and non-terminal
                 node = node.SelectChild()
                 state.apply_move(node.move)
             # Expand
-            if node.untriedMoves:# if we can expand (i.e. state/node is non-terminal)
-                m = random.choice(node.untriedMoves)
-                state.apply_move(m)
-                node = node.expand(m,state)# add child and descend tree
-            # Rollout - this can often be made orders of magnitude quicker using a state.GetRandomMove() function
-            while state.legal_moves():# while state is non-terminal
-                state.apply_move(random.choice(state.legal_moves()))
+
+            if node.state not in visited:# if we can expand (i.e. state/node is non-terminal)
+                visited.add(node)
+                hero = node.playerJustmoved
+                p,v = nn.predict(node.state)
+                node.expand(p)# add child and descend tree
+            # # Rollout - this can often be made orders of magnitude quicker using a state.GetRandomMove() function
+            # while state.legal_moves():# while state is non-terminal
+            #     state.apply_move(random.choice(state.legal_moves()))
             # Backpropagate
+
             while node:# backpropagate from the expanded node and work back to the root node
-                node.update(1 if state.winner() == node.playerJustMoved else -1)# state is terminal. Update node with result from POV of node.playerJustMoved
+                node.update(v if hero == node.playerJustMoved else -v)# state is terminal. Update node with result from POV of node.playerJustMoved
                 node = node.parentNode
-        return sorted(rootnode.childNodes,key=lambda c: c.visits)[-1].move# return the move that was most visited
+        return sorted(rootnode.childNodes,key=lambda c: c.visits)# return the move that was most visited
 
 
 
