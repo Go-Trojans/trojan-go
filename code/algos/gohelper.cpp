c78efba0400a040d805102cc140da57518d6ad45
CSCI561-Go <rupeshsjce@gmail.com>
2020-08-04 12:34:23 +0530

    commit

--- code/algos/gohelper.cpp
+++ code/algos/gohelper.cpp
@@ -5,3 +5,74 @@ File   : gohelper.cpp
 
 Description : Define Go Domain Rules related helper functions and data-types.
 */
+
+#include <iostream>
+#include <list>
+#include <set>
+#include <vector>
+#include <map>
+#include <iterator>
+#include <algorithm>
+#include <functional>
+#include <tuple>
+
+#include "gohelper.h"
+
+using namespace std;
+
+list<std::pair<int, int>> Point::neighbours()
+{
+    map<int, char> action = mapAction();
+
+    map<char, std::pair<int, int>> directions;
+    list<std::pair<int, int>> neigh;
+    int row, col;
+
+    directions = mapDirections();
+    size_t len = directions.size();
+    for (int i = 1; i <= len; i++)
+    {
+        neigh.push_back(make_pair(point.first + directions[action[i]].first, point.second + directions[action[i]].second));
+        //cout << point.first + directions[action[i]].first << " " << point.second + directions[action[i]].second << endl;
+    }
+
+    for (auto &elm : neigh)
+    {
+        cout << elm.first << " " << elm.second << endl;
+    }
+
+    return neigh;
+}
+
+#ifdef USAGE
+int main()
+{
+
+    list<std::pair<int, int>> neigh;
+
+    neigh = Point(1, 1).neighbours();
+
+    cout << "Print the neighbours here ..." << endl;
+    for (auto &elm : neigh)
+    {
+        cout << elm.first << " " << elm.second << endl;
+    }
+
+    Point p1(1, 1);
+    Point p2(1, 1);
+
+    if (p1 == p2)
+    {
+        cout << "both points are equal" << endl;
+    }
+    else
+    {
+        cout << "both points are not equal" << endl;
+    }
+
+    Player player(BLACK);
+    cout << "OPPOSITION : " << player.opp() << endl;
+
+    return 0;
+}
+#endif
--- code/algos/gohelper.h
+++ code/algos/gohelper.h
@@ -1,7 +1,94 @@
 /*
-Author :  
+Author : Rupesh Kumar 
 Date   : July 30th 2020
 File   : gohelper.h
 
 Description :  Define header files and function prototypes.
 */
+
+#ifndef GOHELPER_H
+#define GOHELPER_H
+
+#include <iostream>
+#include <map>
+#include <list>
+
+using namespace std;
+
+#define BLACK 1
+#define WHITE 2
+
+class Player
+{
+public:
+    int color;
+    Player(int color)
+    {
+        this->color = color;
+    }
+    int opp()
+    {
+        return (this->color == WHITE) ? BLACK : WHITE;
+    }
+};
+
+#ifdef USAGE
+int main()
+{
+    Player player(BLACK);
+    cout << player.opp() << endl;
+    return 0;
+}
+#endif
+
+map<char, std::pair<int, int>> mapDirections()
+{
+    map<char, std::pair<int, int>> directions;
+    directions[{'W'}] = make_pair(-1, 0);
+    directions[{'E'}] = make_pair(1, 0);
+    directions[{'S'}] = make_pair(0, -1);
+    directions[{'N'}] = make_pair(0, 1);
+
+    //cout << directions[{'W'}].first << endl;
+
+    return directions;
+}
+map<int, char> mapAction()
+{
+    map<int, char> action;
+    action.insert(make_pair(1, 'W'));
+    action.insert(make_pair(2, 'E'));
+    action.insert(make_pair(3, 'S'));
+    action.insert(make_pair(4, 'N'));
+
+    return action;
+}
+
+class Point
+{
+public:
+    std::pair<int, int> point;
+
+    Point(int row, int col)
+    {
+        point.first = row;
+        point.second = col;
+    }
+
+    list<std::pair<int, int>> neighbours();
+
+    bool operator==(const Point &other) const
+    {
+        return (point.first == other.point.first && point.second == other.point.second);
+    }
+};
+
+/*
+ Function prototypes.
+ */
+#ifdef USAGE
+// Internall it uses find_connected(board, point, player). Define both functions in gohelper.cpp
+bool is_point_an_eye(GoBoard *board, Point point, Player player);
+#endif
+
+#endif
