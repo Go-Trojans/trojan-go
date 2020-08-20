9ed95ea0f2b567d6a838e2adaa31cf5a677341c0
CSCI561-Go <rupeshsjce@gmail.com>
2020-08-20 14:07:54 +0530

    commit

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Check and set the CUDA 10.1
+ls -l /usr/local/cuda
+sudo rm -rf /usr/local/cuda
+sudo ln -s /usr/local/cuda-10.1 /usr/local/cuda
+
+# Create the project by pulling it from git.
+mkdir TROJANGO; cd TROJANGO
+git clone https://CSCI561-Go:TrojanGousc123@github.com/Go-Trojans/trojan-go.git --branch aws_trojango
+cd
+
+# Set the conda environment.
+conda create --name awsgo --file TROJANGO/trojan-go/spec-file.txt
+source activate awsgo
+pip install tensorflow-gpu==2.2
+
+# Run the code.
+cd TROJANGO/trojan-go/code/
+for a in /sys/bus/pci/devices/*; do echo 0 | sudo tee -a $a/numa_node; done
+python3 selfplay_train_eval_sync.py --no-production
